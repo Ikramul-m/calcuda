41517a3ee55d60fbc8f52a9ed6d659d0e5daf2fd
Ikramul-m <i20727172@gmail.com>
2024-03-28 16:52:10 +0530

    lesson

---
+++ array.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] ar = { 21, 22, 24, 10, 5, 90 };
+
+        for (int i = 0; i < ar.Length; i++)
+        {
+            Console.Write(ar[i] + "  ");
+        }
+    }
+}
+
+
+
+
+
+
+
+
