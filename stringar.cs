f3a6e1212eaeee2cc44de2e983f6442cb0e418c2
Ikramul-m <i20727172@gmail.com>
2024-03-28 17:01:00 +0530

    lesson

--- stringar.cs
+++ stringar.cs
@@ -4,18 +4,13 @@ class Program
 {
     public static void Main(string[] args)
     {
-        string[] arr = { "ikram", "ifdal", "antara" };
+        string[] arr = { "ikram", "ifdal", "antara", "boin", "bhai", "bhai" };
 
         for (int i = 0; i < arr.Length; i++)
         {
-            Console.Write(arr[i] + "    ");
+            Console.Write(arr[i] + " ");
         }
 
     }
 }
-
-
-
-
-
 
