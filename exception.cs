fd5e66b5b3f47d28c56c73bd7eb805eabc9c5d5d
Ikramul-m <i20727172@gmail.com>
2024-03-28 18:41:43 +0530

    lesson

---
+++ exception.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Enter a number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The result is: " + (x / y));
+        }
+        catch
+        {
+            Console.WriteLine("Error");
+        }
+    }
+}
+
