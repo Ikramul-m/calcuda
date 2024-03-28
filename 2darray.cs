887bb4b18ac503b745dc200c8a96b237b352d53c
Ikramul-m <i20727172@gmail.com>
2024-03-28 18:29:30 +0530

    lesson

--- 2darray.cs
+++ 2darray.cs
@@ -5,18 +5,18 @@ class Program
     public static void Main(string[] args)
     {
         int[,] arr = {
-            {1,2,4},
-            {3, 4,54},
-            {5,6,45}
+            {2 , 4 , 6 , 8},
+            {1 , 3 , 5 , 7}
         };
 
         for (int i = 0; i < arr.GetLength(0); i++)
         {
             for (int j = 0; j < arr.GetLength(1); j++)
             {
-                Console.Write(arr[i, j] + " ");
+                Console.Write(arr[i, j] + "   ");
             }
             Console.WriteLine();
         }
     }
 }
+
