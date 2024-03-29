1240cb118ef124e3a25905772fe0719d2dcb6450
Ikramul-m <i20727172@gmail.com>
2024-03-29 12:11:10 +0530

    lesson

---
+++ constructor.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Book
+{
+    public string title;
+    public string author;
+    public int pages;
+
+    public Book(string aTitle, string aAuthor, int aPages)
+    {
+        title = aTitle;
+        author = aAuthor;
+        pages = aPages;
+    }
+
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Book book1 = new Book("John Wick", "Ikram", 400);
+        Console.WriteLine(book1.title);
+    }
+}
+
+
