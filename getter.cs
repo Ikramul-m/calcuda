b01404e460b2635f30dac695618f0cb23a4672b7
Ikramul-m <i20727172@gmail.com>
2024-03-29 21:24:23 +0530

    lesson

---
+++ getter.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Movie
+{
+    public string name;
+    public string director;
+    private string rating;
+
+    public Movie(string aName, string aDirector, string aRating)
+    {
+        name = aName;
+        director = aDirector;
+        rating = aRating;
+    }
+
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Movie movie1 = new Movie("Aniaml", "James", "PG");
+        Console.Write(movie1.rating);
+    }
+}
+
+
+
