79496954d14207c57a85a8611ff5bf55062be868
Ikramul-m <i20727172@gmail.com>
2024-03-29 22:06:39 +0530

    lesson

---
+++ staticClsaa.cs
@@ -0,0 +1,30 @@
+using System;
+
+class Song
+{
+    public string title;
+    public string artist;
+    public int duration;
+
+    public Song(string aTitle, string aArtist, int aDuration)
+    {
+        title = aTitle;
+        artist = aArtist;
+        duration = aDuration;
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Song holiday = new Song("Holiday", "Green Day", 200);
+        Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
+
+        Console.WriteLine(holiday.title);
+        Console.WriteLine(kashmir.title);
+    }
+}
+
+
+
